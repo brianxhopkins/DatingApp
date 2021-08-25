6a91f2b618a28a94d4ff8d68bb8a343fbf107e31
brianxhopkins <brianhopkins262@gmail.com>
2021-08-25 15:21:15 -0400

    Added "Likes" Table to the db

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -7,8 +7,34 @@ namespace API.Data
     {
         public DataContext(DbContextOptions options) : base(options)
         {
+
         }
 
         public DbSet<AppUser> Users { get; set; }
+        public DbSet<UserLike> Likes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder) {
+            base.OnModelCreating(builder);
+
+            // Forms primary key for "Liked" table
+            builder.Entity<UserLike>()
+                .HasKey(k => new {k.SourceUserId, k.LikedUserId});
+
+            // Relationships
+            builder.Entity<UserLike>()
+                .HasOne(s => s.SourceUser)
+                .WithMany(l => l.LikedUsers)
+                .HasForeignKey(s => s.SourceUserId)
+                // Must change to DeleteBehavior.NoAction if switching
+                // to SQL Server
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.Entity<UserLike>()
+                .HasOne(s => s.LikedUser)
+                .WithMany(l => l.LikedByUsers)
+                .HasForeignKey(s => s.LikedUserId)
+                // SQL Server command listed above
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -10,6 +10,7 @@ namespace API.Entities
         public string UserName { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        
         public DateTime DateOfBirth { get; set; }
         public string KnownAs { get; set; }
         public DateTime Created { get; set; } = DateTime.Now;
@@ -20,11 +21,10 @@ namespace API.Entities
         public string Interests { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
+
         public ICollection<Photo> Photos { get; set; }
 
-        // public int GetAge()
-        // {
-        //     return DateOfBirth.CalculateAge();
-        // }
+        public ICollection<UserLike> LikedByUsers { get; set; }
+        public ICollection<UserLike> LikedUsers { get; set; }
     }
 }
