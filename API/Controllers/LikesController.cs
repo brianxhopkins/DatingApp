3a3a1c31ae28555f753770cf705c0a0e6b48781e
brianxhopkins <brianhopkins262@gmail.com>
2021-09-01 18:08:13 -0400

    Began adding "like" feature

---
+++ API/Controllers/LikesController.cs
@@ -0,0 +1,37 @@
+using System.Threading.Tasks;
+using API.Extensions;
+using API.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Authorize]
+    public class LikesController : BaseApiController
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly ILikesRepository _likesRepository;
+        public LikesController(IUserRepository userRepository, ILikesRepository likesRepository)
+        {
+            _likesRepository = likesRepository;
+            _userRepository = userRepository;
+        }
+
+        [HttpPost("{username}")]
+        public async Task<ActionResult> AddLike(string username) {
+            var sourceUserId = User.GetUserId();
+            var likedUser = await _userRepository.GetUserByUsernameAsync(username);
+            var sourceUser = await _likesRepository.GetUserWithLikes(sourceUserId);
+
+            if (likedUser == null) {
+                return NotFound();
+            }
+
+            if (sourceUser.UserName == username) {
+                return BadRequest("You cannot like yourself");
+            }
+
+            
+        }
+    }
+}
